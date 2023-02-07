9a0044f65a957720a60bb45a2f84160f839f0c2f
Léonard Krief <53581910+leonardkrief@users.noreply.github.com>
2023-02-07 14:05:35 +0100

    la belle sphere bien brillante arrive vous la voyez ?

--- srcs/main.c
+++ srcs/main.c
@@ -6,121 +6,68 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 18:48:55 by lkrief            #+#    #+#             */
-/*   Updated: 2023/02/06 17:01:31 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/02/07 13:55:39 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-void	put_point(t_image *image, t_tuple pix)
+int	main(void)
 {
-	int	i;
-	int	j;
+	//************************LIGHTING**********************//
+	
+	printf("\n********************TEST 1**********************\n");
+	t_material	m = material();
+	t_tuple		position = point(0, 0, 0);
+	t_tuple		eyev = vector(0, 0, -1);
+	t_tuple		normalv = vector(0, 0, -1);
+	t_light		light = point_light(point(0, 0, -10), tmp_pixel(TMP_PIXEL_WHITE, 0));
+	t_tmp_pixel	result = lighting(m, light, position, eyev, normalv);
+	t_tmp_pixel	exp = tmp_pixel(1.9, 1.9, 1.9, 0);
+	tmp_pixel_print(result, "p1:   ");
+	tmp_pixel_print(exp, "exp1: ");
 
-	pix = point_to_pixel(image, pix.x, pix.y);
-	i = pix.x - 6;
-	while (++i < pix.x + 5)
-	{
-		if (i >= 0 && i < WIDTH)
-		{
-			j = pix.y - 6;
-			while (++j < pix.y + 5)
-			{
-				if (j >= 0 && j < HEIGHT)
-					put_pixel(image, i, j, pixel(255, 0, 255, 0));
-			}
-		}
-	}
+	printf("\n********************TEST 2**********************\n");
+	m = material();
+	position = point(0, 0, 0);
+	eyev = vector(0, sqrt(2)/2, -sqrt(2)/2);
+	normalv = vector(0, 0, -1);
+	light = point_light(point(0, 0, -10), tmp_pixel(TMP_PIXEL_WHITE, 0));
+	result = lighting(m, light, position, eyev, normalv);
+	exp = tmp_pixel(1, 1, 1, 0);
+	tmp_pixel_print(result, "p2:   ");
+	tmp_pixel_print(exp, "exp1: ");
+
+	printf("\n********************TEST 3**********************\n");
+	m = material();
+	position = point(0, 0, 0);
+	eyev = vector(0, 0, -1);
+	normalv = vector(0, 0, -1);
+	light = point_light(point(0, 10, -10), tmp_pixel(TMP_PIXEL_WHITE, 0));
+	result = lighting(m, light, position, eyev, normalv);
+	exp = tmp_pixel(0.7364, 0.7364, 0.7364, 0);
+	tmp_pixel_print(result, "p3:   ");
+	tmp_pixel_print(exp, "exp1: ");
+
+	printf("\n********************TEST 4**********************\n");
+	m = material();
+	position = point(0, 0, 0);
+	eyev = vector(0, -sqrt(2)/2, -sqrt(2)/2);
+	normalv = vector(0, 0, -1);
+	light = point_light(point(0, 10, -10), tmp_pixel(TMP_PIXEL_WHITE, 0));
+	result = lighting(m, light, position, eyev, normalv);
+	exp = tmp_pixel(1.6364, 1.6364, 1.6364, 0);
+	tmp_pixel_print(result, "p4:   ");
+	tmp_pixel_print(exp, "exp1: ");
+
+	printf("\n********************TEST 5**********************\n");
+	m = material();
+	position = point(0, 0, 0);
+	eyev = vector(0, 0, -1);
+	normalv = vector(0, 0, -1);
+	light = point_light(point(0, 0, 10), tmp_pixel(TMP_PIXEL_WHITE, 0));
+	result = lighting(m, light, position, eyev, normalv);
+	exp = tmp_pixel(0.1, 0.1, 0.1, 0);
+	tmp_pixel_print(result, "p5:   ");
+	tmp_pixel_print(exp, "exp1: ");
 }
-
-// ecran dans le plan (1,0,0)(0,1,0) qui contient le point (0,0,-3)
-// je me situe en (0,0,-5)
-// je dois lancer les rayons avec un angle qui varie:
-// direction (0,0,1) qui doit aller toucher chacun des pixels
-// i determine l'angle u dans le plan xz
-// j determine l'angle v dans le plan yz
-
-void	put_raytracer_to_image(t_image *img, t_raytracer rt, int i, int j)
-{
-	if (rt.itr_front.t > 0)
-		put_pixel(img, i, j, pixel(255, 0, 255, 0));
-}
-
-#define MACRO_SIZE 1
-
-void	make_scene(t_canvas *cvs)
-{
-	int	i, j;
-	t_tuple a;
-	t_tuple light;
-	t_raytracer	rt;
-	t_matrix	m = matrix_scaling(MACRO_SIZE, MACRO_SIZE, MACRO_SIZE);
-	t_sphere	*sp = sphere(point(0, 0, 0), 1);
-	transform_sp(sp, &m);
-
-	light = point(0, 0, -5);
-	for(i = 0; i < WIDTH; i++)
-	{
-		for(j = 0; j < HEIGHT; j++)
-		{
-			a = pixel_to_point(&cvs->image, i, j);
-			a = tuple_sub(a, light);
-			a = normalize(a);
-			// printf("(i j) = (%2d %2d)     (x y z) = (%.2f %.2f %.2f)\n", i, j, a.x, a.y, a.z);
-			rt = raytracer(light, a);
-			intersection_sp(&rt, sp);
-			if (rt.itr_front.id != 0)
-				put_pixel(&cvs->image, i, j, pixel(255, 0, 255, 0));
-		}
-	}
-}
-
-int	main(int ac, char **av)
-{
-	(void)ac;
-	(void)av;
-
-	t_canvas	cvs;
-	// t_raytracer	rt  = raytracer(point(0, 0, -5), vector(0, 0, 1));
-	t_sphere	*sp = sphere(point(0, 0, 0), 1);
-	t_matrix	m;
-	// (void)rt;
-	(void)cvs;
-	(void)sp;
-	(void)m;
-
-	canvas(&cvs);
-	make_scene(&cvs);
-	// sphere_print((const t_sphere) *sp, "S1: ");
-	// (void)m;
-	// m = matrix_scaling(2, 2, 2);
-	// transform_sp(sp, &m);
-
-	mlx_put_image_to_window(cvs.window.mlx, cvs.window.win, cvs.image.img, 0, 0);
-	mlx_loop(cvs.window.mlx);
-}
-
-
-
-// int	main(void)
-// {
-// 	t_raytracer	rt  = raytracer(point(0, 0, -5), vector(0, 0, 1));
-// 	t_sphere	*sp;
-// 	(void)rt;
-
-// 	sp = sphere(point(0, 0, 0), 1);
-// 	sphere_print((const t_sphere) *sp, "S1: ");
-// 	t_matrix	m;
-// 	(void)m;
-// 	// m = matrix_scaling(2, 2, 2);
-// 	m = matrix_translation(2, 0, 0);
-// 	transform_sp(sp, &m);
-// 	sphere_print(*sp, "S2: ");
-// 	intersection_sp(&rt, sp);
-// 	raytracer_print(rt, "R1: ");
-// 	// sp = sphere(point(0, 0, -5), 1);
-// 	// intersection_sp(&rt, sp);
-// 	// raytracer_print(rt, "R2: ");
-// 	// intersection_sp(r, sp);
-// 	// printf("%lu %lu %lu\n", sizeof(itr), sizeof(t_cylinder), sizeof(t_triangle));
-// }
