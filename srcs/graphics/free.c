1b06317d6157edf7c482b6126675e375ffb553c1
Leonard KRIEF <lkrief@e2r5p5.clusters.42paris.fr>
2023-02-08 06:45:44 +0100

    correctly frees mlx

--- srcs/graphics/free.c
+++ srcs/graphics/free.c
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 20:41:24 by lkrief            #+#    #+#             */
-/*   Updated: 2023/02/07 12:49:24 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/02/08 04:04:08 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	free_window(const t_window *window)
 {
 	mlx_destroy_window(window->mlx, window->win);
+	mlx_destroy_display(window->mlx);
 	free(window->mlx);
 }
 
@@ -23,8 +24,9 @@ void	free_image(const t_image *image)
 	mlx_destroy_image(image->mlx, image->img);
 }
 
-void	free_canvas(const t_canvas *canvas)
+int	free_canvas(const t_canvas *canvas)
 {
 	free_image(&canvas->image);
 	free_window(&canvas->window);
+	return (0);
 }
--- srcs/graphics/graphics.c
+++ srcs/graphics/graphics.c
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 22:59:13 by lkrief            #+#    #+#             */
-/*   Updated: 2023/02/06 16:52:33 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/02/08 02:48:10 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_window	*window(t_window *window)
 	window->win = mlx_new_window(window->mlx, WIDTH, HEIGHT, WINDOW_NAME);
 	if (window->win == NULL)
 	{
-		free(window->mlx);
+		mlx_destroy_display(window->mlx);
 		return (NULL);
 	}
 	return (window);
