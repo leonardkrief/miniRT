b4e50d933c359c6e92d81f25d66dc0de0396ddb0
Leonard KRIEF <lkrief@e2r5p5.clusters.42paris.fr>
2023-02-08 06:47:03 +0100

    sp

--- srcs/objects/sphere.c
+++ srcs/objects/sphere.c
@@ -6,7 +6,7 @@
 /*   By: lkrief <lkrief@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 13:09:37 by lkrief            #+#    #+#             */
-/*   Updated: 2023/02/07 13:21:19 by lkrief           ###   ########.fr       */
+/*   Updated: 2023/02/07 20:52:34 by lkrief           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,3 +47,30 @@ void	transform_sp(t_sphere *sp, const t_matrix *m)
 	sp->t_m = matrix_transpose(sp->m);
 }
 
+t_tuple	normal_at_sp(const t_sphere *sp, t_tuple t)
+{
+	t = matrix_vect(sp->m, t);
+	t.w = 0;
+	t = matrix_vect(sp->t_m, t);
+	t.w = 0;
+	return (tuple_normalize(t));
+}
+
+void	sphere_print_padding(const t_sphere sphere, const char *name, const int padding)
+{
+	int	len;
+	t_tuple	t;
+
+	len = ft_strlen(name);
+	printf("%*s%ssph: { origin:\t", padding, "", name);
+	t = matrix_vect(matrix_invert(sphere.m, 4), sphere.origin);
+	tuple_raw_print(t);
+	printf(",\n%*s", padding, "");
+	printf("%*sradius:\t% .2f\n%*s", len + 7, "", sphere.radius, padding, "");
+	printf("%*s}\n", len + 5, "");
+}
+
+void	sphere_print(const t_sphere sphere, const char *name)
+{
+	sphere_print_padding(sphere, name, 0);
+}
